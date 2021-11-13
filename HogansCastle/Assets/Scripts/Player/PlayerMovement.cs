fef811273659c6fda50a5fc39b1e308019baf2ae
xJeevz <starjeevz2001@gmail.com>
2021-11-13 18:47:58 -0500

    Added Terry

---
+++ HogansCastle/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,157 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Variables
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float runSpeed;
+    [SerializeField] private float rotationSpeed;
+    private SceneLoader sceneLoader = new SceneLoader();
+    private float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+
+    private Vector3 moveDirection;
+    private Vector3 velocity;
+
+    [SerializeField] private bool isGrounded;
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private float gravity;
+
+    [SerializeField] private float jumpHeight;
+    [SerializeField] private float coolDown = 5f;
+    [SerializeField] private float coolDownTimer = 0;
+
+    // References
+    private CharacterController controller;
+    private Animator anim;
+    public Transform cam;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        controller = GetComponent<CharacterController>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        Move();
+        if (coolDownTimer > 0)
+        {
+            coolDownTimer -= Time.deltaTime;
+        }
+        if (coolDownTimer < 0)
+        {
+            coolDownTimer = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse0) && coolDownTimer == 0)
+        {
+            StartCoroutine(Attack());
+            coolDownTimer = coolDown;
+        }
+        
+    }
+
+    private void Move()
+    {
+        isGrounded = Physics.CheckSphere(transform.position, groundCheckDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0) 
+        {
+            velocity.y = -2f;
+        }
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveZ = Input.GetAxisRaw("Vertical");
+        moveDirection = new Vector3(moveX, 0, moveZ).normalized;
+        //moveDirection = transform.TransformDirection(moveDirection);
+
+        if (isGrounded)
+        {
+            if (moveDirection != Vector3.zero && !Input.GetKey(KeyCode.LeftShift))
+            {
+                Walk();
+            }
+            else if (moveDirection != Vector3.zero && Input.GetKey(KeyCode.LeftShift))
+            {
+                Run();
+            }
+            else if (moveDirection == Vector3.zero)
+            {
+                Idle();
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                StartCoroutine(Jump());
+            }
+        }
+        if (moveDirection == Vector3.zero)
+        {
+            controller.Move(moveDirection * moveSpeed * Time.deltaTime);
+            velocity.y += gravity * Time.deltaTime;
+            controller.Move(velocity * Time.deltaTime);
+        }
+        else
+        {
+            float targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * moveSpeed * Time.deltaTime);
+            velocity.y += gravity * Time.deltaTime;
+            controller.Move(velocity * Time.deltaTime);
+        }
+        
+
+    }
+
+    private void Idle()
+    {
+        moveSpeed = 0;
+        anim.SetFloat("Speed", 0.0f, 0.1f, Time.deltaTime);
+    }
+
+    private void Walk()
+    {
+        moveSpeed = walkSpeed;
+        anim.SetFloat("Speed", 0.5f, 0.1f, Time.deltaTime);
+    }
+
+    private void Run()
+    {
+        moveSpeed = runSpeed;
+        anim.SetFloat("Speed", 1, 0.1f, Time.deltaTime);
+    }
+
+    private IEnumerator Jump()
+    {
+        anim.SetTrigger("Jump");
+        yield return new WaitForSeconds(0.1f);
+        velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.CompareTag("Finish")) {
+            Debug.Log("Collided");
+            sceneLoader.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+    private IEnumerator Attack()
+    {
+
+        //anim.SetLayerWeight(anim.GetLayerIndex("Attack Layer"), 1);
+        anim.SetTrigger("Attack");
+
+        yield return new WaitForSeconds(0.1f);
+       // anim.SetLayerWeight(anim.GetLayerIndex("Attack Layer"), 0);
+    }
+}
